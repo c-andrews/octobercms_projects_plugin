f9cbb96f8b78ddd1345131bec795b92700b88427
c-andrews <chris@c-andrews.co.uk>
2016-06-07 11:16:55 +0100

    Updated Re-order in Project
    Updated the reorder functionality in the project

--- chrisandrews/work/models/Project.php
+++ chrisandrews/work/models/Project.php
@@ -7,7 +7,7 @@ use Model;
  */
 class Project extends Model
 {
-    // use \October\Rain\Database\Traits\Validation;
+use \October\Rain\Database\Traits\Validation;
     use \October\Rain\Database\Traits\Sortable;
 
     /*
